aae9d3cb89614631295bd5a091ff4681e63a3339
ncrespod <77981743+ncrespod@users.noreply.github.com>
2023-11-14 18:09:10 -0500

    fd

--- core/urls.py
+++ core/urls.py
@@ -5,8 +5,6 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.contrib import admin
 from django.urls import path, include  # add this
-from alertas.views import Alertas
-from registros.views import Reg
 
 urlpatterns = [
 
@@ -15,12 +13,4 @@ urlpatterns = [
     path("", include("app.urls")), 
     path('',include('com.urls',namespace='com')),
     path('',include("registros.urls")),
-    
-    
- 
-  
-    
-   
-   
-               # UI Kits Html files
 ]
