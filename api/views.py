68c7bab00b999bde2f08488f3a2980a6156d8070
proyectoard <arduinoproyecto77@gmail.com>
2024-05-14 15:32:45 -0500

    hola

--- api/views.py
+++ api/views.py
@@ -8,7 +8,7 @@ import requests
 def obtener_datos(request):
     api_url = "https://blynk.cloud/external/api/get?token=KZ1_-NaSq8rfh-dBPAXvnLnmzk63GFV8&v0&v1&v2&v3&v4"
     response = requests.get(api_url)
-     
+    #hola
     if response.status_code == 200:
         data = response.json()
           
